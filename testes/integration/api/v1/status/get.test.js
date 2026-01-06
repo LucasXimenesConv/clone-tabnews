254d2655414867c2a3e3dde1e705ecb9489a1f4c
Lucas Ximenes <86805502+LucasXimenesConv@users.noreply.github.com>
2026-01-06 11:46:52 +0000

    add ssl field in

--- testes/integration/api/v1/status/get.test.js
+++ testes/integration/api/v1/status/get.test.js
@@ -8,7 +8,9 @@ test("GET to /api/v1/status should return 200", async () => {
   const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
   expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
 
-  expect(responseBody.dependencies.database.version).toEqual("16.0");
+  expect(responseBody.dependencies.database.version).toEqual(
+    expect.any(String),
+  );
   expect(responseBody.dependencies.database.max_connections).toEqual(
     expect.any(Number),
   );
